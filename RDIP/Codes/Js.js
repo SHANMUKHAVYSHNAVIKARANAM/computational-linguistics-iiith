54fc5c3596f4d395a45a085d68cf1f66de51e0f8
SHANMUKHAVYSHNAVIKARANAM <shanmukhavyshnavi6@gmail.com>
2020-09-14 13:46:58 +0530

    buttonscreated

--- RDIP/Codes/Js.js
+++ RDIP/Codes/Js.js
@@ -63,3 +63,66 @@ var totalh=["राम और श्याम बाजार गयें","र
        sentenceh(h); }
         }
            } 
+            var str = e.split(" ");
+   var suf = shuffle(str);
+   var l = suf.length;
+   var z = 0;
+   function sentence(sen){
+  document.getElementById("o1").style.visibility="hidden";
+  document.getElementById("o2").style.visibility="hidden";
+   document.getElementById("o3").style.visibility="hidden";
+    document.getElementById("op2").innerHTML="";
+    document.getElementById("op4").innerHTML="";
+    buttons(suf ,l);
+    }
+     var strh = h.split(" ");
+   var sufh = shuffle(strh);
+   var lh = sufh.length;
+    function sentenceh(sen){
+  document.getElementById("o1").style.visibility="hidden";
+  document.getElementById("o2").style.visibility="hidden"
+   document.getElementById("o3").style.visibility="hidden";
+   document.getElementById("op2").innerHTML="";
+    document.getElementById("op4").innerHTML="";
+    buttonsh(sufh ,lh);
+    }
+    function buttons(suff,le){
+      g = 1;
+      document.getElementById("op2").innerHTML="";
+    document.getElementById("op4").innerHTML="";
+    for (i =0;i<le;i++){
+                        var b = suff[i]
+                       var btn = document.createElement("button");
+                       var t = document.createTextNode(suff[i]);
+                        btn.id='b'+i;
+                       btn.appendChild(t);
+                       document.body.appendChild(btn);
+                    btn.addEventListener('click', function(suff , le){
+                        document.getElementById("op1").innerHTML = "Formed Sentence(after selecting words)" ;
+          
+    document.getElementById("op2").innerHTML+=this.innerHTML+" ";
+    this.style.display="none";
+     document.getElementById("o1").style.visibility="visible";
+     correct();
+ });}
+ }
+  function buttonsh(suff,le){
+    g = 1;
+    document.getElementById("op2").innerHTML="";
+    document.getElementById("op4").innerHTML="";
+    for (i =0;i<le;i++){
+                        var b = suff[i]
+                       var btn = document.createElement("button");
+                       var t = document.createTextNode(suff[i]);
+                        btn.id='b'+i;
+                       btn.appendChild(t);
+                       document.body.appendChild(btn);
+                    btn.addEventListener('click', function(suff , le){
+                        document.getElementById("op1").innerHTML = "Formed Sentence(after selecting words)" ;
+          
+    document.getElementById("op2").innerHTML+=this.innerHTML+" ";
+    this.style.display="none";
+     document.getElementById("o1").style.visibility="visible";
+      correcth();
+    
+ });}
